98b97ddd75996a96e99ea17efea1465645e95a02
stepanoves <stepanovyahor@gmail.com>
2019-02-19 13:40:12 +0300

    minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
+import { DBUrl } from './DBUrl';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as dbconfig from '../db.config.json';
 import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb://${dbconfig.dbuser}:${dbconfig.dbpassword}@ds141815.mlab.com:41815/internship-crm`), NewsModule],
+  imports: [MongooseModule.forRoot(DBUrl.getUrl('ds141815.mlab.com:41815/internship-crm')), NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
