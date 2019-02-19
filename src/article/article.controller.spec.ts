014ee387c954e7bb8f6cd3ee5a60ee688b9bbf70
stepanoves <stepanovyahor@gmail.com>
2019-02-19 19:11:06 +0300

    fixed test

--- src/article/article.controller.spec.ts
+++ src/article/article.controller.spec.ts
@@ -1,15 +1,7 @@
-import { ConfigService } from './../config.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ArticleModule } from './article.module';
 import { MockArticleService } from './mock/mock.article.service.mock';
 import { Test } from '@nestjs/testing';
-import { Article } from './interfaces/article.interface';
-import { Model } from 'mongoose';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import * as testData from './tests.data.json';
 
 
 describe('ArticleController', () => {
@@ -33,35 +25,5 @@ describe('ArticleController', () => {
     });
 });
 
-describe('ArticleController e2e', () => {
-    let app: INestApplication;
 
-    let articleService = { findAll: () => testData.getArticle};
-
-    beforeAll(async () => {
-        const module = await Test.createTestingModule({
-            controllers: [ArticleController],
-            imports: [MongooseModule.forRootAsync({
-                useClass: ConfigService
-            }), ArticleModule],
-        })
-          .overrideProvider(ArticleService)
-          .useValue(articleService)
-          .compile();
-    
-        app = module.createNestApplication();
-        await app.init();
-      });
-    
-    it(`/GET article`, () => {
-      return request(app.getHttpServer())
-        .get('/article')
-        .expect(200)
-        .expect(articleService.findAll());
-    });
-
-    afterAll(async () => {
-      await app.close();
-    });
-})
 
