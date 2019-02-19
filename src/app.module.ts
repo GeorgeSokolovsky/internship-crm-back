b5a7eb9adc4c8d9258b4d0840ae19deb61816788
stepanoves <stepanovyahor@gmail.com>
2019-02-19 12:35:09 +0300

    added simple api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dbconfig from '../db.config.json';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb://${dbconfig.dbuser}:${dbconfig.dbpassword}@ds141815.mlab.com:41815/internship-crm`), NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
