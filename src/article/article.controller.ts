a7dfdfb63ec1ddc09f8591408740d218cc3abc30
KKurchavy <kkurchavy@mail.ru>
2019-02-26 12:40:43 +0300

    add authorization check when creating and modifying articles and categories

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,18 +1,30 @@
+import {
+  Post,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Query,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ArticleDto } from './dto/ArticleDto';
 import { SearchQuery } from './interfaces/search-query.interface';
 import { ArticleService } from './article.service';
-import { Post, Body, Controller, Get, Param, Query, Put } from '@nestjs/common';
 
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   async create(@Body() articleDto: ArticleDto) {
     this.articleService.create(articleDto);
   }
 
   @Put('/:id')
+  @UseGuards(AuthGuard('jwt'))
   async update(@Param('id') id: string, @Body() articleDto: ArticleDto) {
     this.articleService.update(id, articleDto);
   }
